24312e62a74399bf1d1027f6c36803da5e5bfd32
Bruno <brunocostah@hotmail.com>
2021-04-25 18:15:14 -0300

    parte V conteudo concluida

---
+++ bloco_4/aula_4.1/Conteudo/scripts4.js
@@ -0,0 +1,18 @@
+let candidato = 'Lista';
+
+switch (candidato) {
+    case 'aprovado':
+        console.log('Aprovado');
+        break;
+
+    case 'reprovado':
+        console.log('reprovado')
+        break;
+    
+    case 'Lista':
+        console.log('Lista');
+        break;
+
+    default:
+        console.log('não se aplica');
+}
