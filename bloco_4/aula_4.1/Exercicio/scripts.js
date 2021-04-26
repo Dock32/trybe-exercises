d34284883071d8525e0cbcaf3787b84602c46b78
Bruno <brunocostah@hotmail.com>
2021-04-26 00:46:39 -0300

    Exercicio 10 feito

--- bloco_4/aula_4.1/Exercicio/scripts.js
+++ bloco_4/aula_4.1/Exercicio/scripts.js
@@ -157,3 +157,14 @@ if(namberA && namberB && namberC %2 !== 0) {
 
 /*exercicio 10*/ 
 
+
+let costOfProduct = 1;
+let saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  let totalCostOfProduct = costOfProduct * 1.2;
+  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
