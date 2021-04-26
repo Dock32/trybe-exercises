9a3ebc669d0c127cba8cfc1e739890db8f9fe609
Bruno <brunocostah@hotmail.com>
2021-04-25 22:47:23 -0300

    Exercicio 5 e 6  feito

--- bloco_4/aula_4.1/Exercicio/scripts.js
+++ bloco_4/aula_4.1/Exercicio/scripts.js
@@ -50,3 +50,51 @@ else{
 }
 
 /*exercicio 5*/
+
+let resultadoA = 5;
+let resultadoB = 4;
+let resultadoC = 9;
+
+if (resultadoA < resultadoB + resultadoC && resultadoB < resultadoA + resultadoC && resultadoC < resultadoB + resultadoA) {
+    console.log('true');
+}
+
+else {
+    console.log(false);
+}
+
+/*exercicio 6*/
+
+let pecaXadrex = 'Dama'.toLowerCase();
+
+switch (pecaXadrex) {
+    case 'cavalo':
+        console.log('Salto em L.');
+        break;
+
+        case 'bispo':
+            console.log('Diagonais.');
+            break;
+        
+        case 'torre':
+            console.log('Cruz.');
+            break;
+
+        case 'dama':
+            console.log('Retas pelas fileiras, colunas e diagonais.');
+            break;
+
+        case 'rei':
+            console.log('Uma casa em volta de si.');
+            break;
+
+        case 'peão':
+            console.log('Move-se para frente.');
+            break;
+
+        default :
+            console.log('Essa peça não existe.');
+            break;
+}
+
+/*exercicio 7*/
