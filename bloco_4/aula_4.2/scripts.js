84c8bcd5592b4507dcc9cdfc488d25c9737d8bdb
Bruno <brunocostah@hotmail.com>
2021-04-27 00:44:26 -0300

    exercicio 1 a 4 aula  4.2

---
+++ bloco_4/aula_4.2/scripts.js
@@ -0,0 +1,18 @@
+/*exercicio 4*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0;index < numbers.length; index +=1){
+    soma += numbers[index];
+
+    let total = soma / numbers.length;
+
+if (total > 20){
+    console.log('Valor maior que 20');
+}
+else{
+    console.log('Valor menos ou igual a 20');
+};
+
+};
+
